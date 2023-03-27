47033d9a241ce10deed8480d1d525b695390ab96
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-03-28 00:41:11 +0900

    feat: Add memory

---
+++ src/computer/Memory.java
@@ -0,0 +1,37 @@
+package computer;
+
+import java.util.ArrayList;
+import java.util.List;
+import computer.processor.Register;
+
+public class Memory {
+
+  private Register mar;
+  private Register mbr;
+
+  List<Integer> memory;
+
+  public Memory() {
+    this.memory = new ArrayList<>();
+  }
+
+  public void setMemory(List<Integer> memory) {
+    this.memory = memory;
+  }
+
+  public void associate(Register mar, Register mbr) {
+    this.mar = mar;
+    this.mbr = mbr;
+  }
+
+  public void load() {
+    int address = mar.getValue();
+    mbr.setValue(this.memory.get(address));
+  }
+
+  public void store() {
+    int address = mar.getValue();
+    int value = mbr.getValue();
+    this.memory.set(address, value);
+  }
+}
